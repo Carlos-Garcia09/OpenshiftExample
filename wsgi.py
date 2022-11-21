d383280b44416dacc9794f6a9b3dc3109477733d
Carlos-Garcia09 <75263278+Carlos-Garcia09@users.noreply.github.com>
2022-11-21 11:03:01 -0600

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,8 @@ from flask import Flask
 import datetime
 hora = datetime.datetime.now().hour
 minuto = datetime.datetime.now().minute
-exact_hora="La hora del dia de hoy es: " + str(hora) + ":" + str(minuto) + "hrs"+"De la fecha "+ str(fecha)
+fecha = datetime.datetime.now().strftime("%d/%m/%Y")
+exact_hora="La hora del dia de hoy es: " + str(hora) + ":" + str(minuto) + "hrs"+" De la fecha "+ str(fecha)
 application = Flask(__name__)
 @application.route("/")
 def hello():
